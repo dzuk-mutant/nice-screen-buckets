f1cf0737d8c761f068e5aa19923f958e4e4aeff9
Dzuk <dzuk@nope.nope>
2020-12-15 17:16:56 +0000

    Elm package preparation - Completely streamlined and completed documentation. - Changed broad width bucket function and type names to be explicit about the fact they are broad buckets. - Exposed WidthMetrics and HeightMetrics so a user can see what's fully inside Metrics. - Changed readme and license naming in preparation for elm package publishing.

--- src/Screen.elm
+++ src/Screen.elm
@@ -1,14 +1,15 @@
 module Screen exposing  ( Metrics
+                        , WidthMetrics
+                        , HeightMetrics
                         , zero
                         , set
 
-                        , WidthBucket(..)
-                        , FineWidthBucket(..)
-                        , toWidthBucket
-                        , toFineWidthBucket
-                        , HeightBucket(..)
-                        , toHeightBucket
 
+
+                        , BroadWidthBucket(..)
+                        , FineWidthBucket(..)
+                        , HeightBucket(..)
+                        
                         , handset1
                         , handset2
                         , handset3
@@ -26,9 +27,15 @@ module Screen exposing  ( Metrics
                         , medium
                         , tall
 
+
+
                         , Bucket
                         , Boundary(..)
                         , BucketAxis(..)
+
+                        , toBroadWidthBucket
+                        , toFineWidthBucket
+                        , toHeightBucket
 
                         , handset1Bkt
                         , handset2Bkt
@@ -51,12 +58,18 @@ module Screen exposing  ( Metrics
 {-| A module for storing and interpreting screen dimensions using buckets.
 
 Depending on the situation you need, you can either implement screen
-size-based functionality in the form of CSS Media Queries or in Elm code 
-with the bucket types and tools.
+size-based functionality in the form of:
 
-Either form uses the same core set of display buckets, but Elm Data is
-slower because it relies on JavaScript. So when doing style-related
-screen things, always use the Media Queries wherever possible.
+
+- CSS Media Queries in elm-css.
+- Elm code with the data and data conversion.
+- Your own implementation using the raw bucket data.
+
+
+All methods use the same core set of display buckets,
+but doing anything with data in Elm is slower because it relies
+on JavaScript. So when doing presentation-related things,
+always use the Media Queries wherever possible.
 
 
 ----
@@ -67,24 +80,43 @@ Use this array of tools when you need to store and update the screen size
 in your model, so you can change your app's functionality based on screen size.
 
 ## General screen data
-@docs Metrics
+@docs Metrics, WidthMetrics, HeightMetrics
 
 ## Initialising and updating Metrics
 @docs zero, set
 
-## Bucket types and conversions
+## Bucket types
+These are stored in `Screen.Metrics` and can also be converted to from raw
+Ints from functions in the raw numbers section.
 
-### Width
-@docs WidthBucket, FineWidthBucket, toWidthBucket, toFineWidthBucket
+These are ideal for using buckets for changing the basic functionality
+and elements of your app based on screen size. If you want to change
+your app's presentation, you should use CSS Media Queries instead.
 
-### Height
-@docs HeightBucket, toHeightBucket
+```
+Html.div
+    []
+    -- show mobile menu if screen width is Handset
+    [   ( if model.screen.width.broad == Handset then
+            Html.div [] [Html.text "menu"]
+          else
+            []
+        )   
+    ]
+```
+
+@docs BroadWidthBucket, FineWidthBucket, HeightBucket
 
 ----
 
 # CSS Media Queries
 
 Use these to create Media Queries to change your app's presentation in elm-css.
+
+This is the most responsive way to change stuff based on screen size in the
+browser, so you should use it wherever you can.
+
+You can stack these media queries in a list like below:
 
 ```
 css [ withMedia [ Screen.handset, Screen.portable1, Screen.portable2 ]
@@ -103,15 +135,21 @@ css [ withMedia [ Screen.handset, Screen.portable1, Screen.portable2 ]
 @docs handset, portable, wide
 
 ## Width Buckets (fine)
-@docs handset1, handset2, handset3, portable1, portable2, portable3, wide1, wide2, wide3
+@docs handset1, handset2, handset3, portable1, portable2, portable3, wide1, wide2
 
 ## Height Buckets
 @docs limited, medium, tall
 
 ----
 
-# Raw numbers
+# Raw data
 Should you need access to the base metric numbers these buckets are made of.
+
+## Data types
+@docs Bucket, Boundary, BucketAxis
+
+## Convert data to buckets
+@docs toBroadWidthBucket, toFineWidthBucket, toHeightBucket
 
 ## Width Buckets (broad)
 @docs handsetBkt, portableBkt, wideBkt
@@ -121,12 +159,6 @@ Should you need access to the base metric numbers these buckets are made of.
 
 ## Height
 @docs limitedBkt, mediumBkt, tallBkt
-
-----
-
-# Internal data types
-
-@docs Bucket, Boundary, Axis
 
 
 
@@ -139,38 +171,11 @@ import Css exposing (px)
 import Css.Media as Media exposing (screen, only)
 import Svg.Styled exposing (set)
 
-{-| Makes a starting metrics structure where all the values are 0.
-
-Use this in your initial model, but the model must be properly
-initialised afterwards.
--}
-zero : Metrics
-zero =
-    { width =   { exact = 0
-                , broad = toWidthBucket 0
-                , fine = toFineWidthBucket 0
-                }
-    , height =  { exact = 0
-                , broad = toHeightBucket 0
-                }
-    }
-
 
 
 ---------------------- data storage and updating ----------------------
 
 {-| The type for storing all screen information.
-
-```
-{ width :   { exact : Int
-            , broad : WidthBucket
-            , fine : FineWidthBucket
-            }
-, height :  { exact : Int
-            , broad : HeightBucket
-            }
-}
-```
 
 Widths are generally more important than height for determining
 screen layout so there's both broad and fine buckets for width.
@@ -189,7 +194,7 @@ screen layout so there's both broad and fine buckets.
 -}
 type alias WidthMetrics =
     { exact : Int
-    , broad : WidthBucket
+    , broad : BroadWidthBucket
     , fine : FineWidthBucket
     }
 
@@ -201,6 +206,26 @@ screen layout so there's only broad buckets.
 type alias HeightMetrics =
     { exact : Int
     , broad : HeightBucket
+    }
+
+
+
+
+
+{-| Makes a starting metrics structure where all the values are 0.
+
+Use this in your initial model, but the model must be properly
+initialised afterwards.
+-}
+zero : Metrics
+zero =
+    { width =   { exact = 0
+                , broad = toBroadWidthBucket 0
+                , fine = toFineWidthBucket 0
+                }
+    , height =  { exact = 0
+                , broad = toHeightBucket 0
+                }
     }
 
 
@@ -236,7 +261,7 @@ setWidth w metrics =
             newWidth =
                 metrics.width
                 |> (\x -> { x | exact = w })
-                |> (\b -> { b | broad = toWidthBucket w })
+                |> (\b -> { b | broad = toBroadWidthBucket w })
                 |> (\b -> { b | fine = toFineWidthBucket w })
         in
             { metrics | width = newWidth }
@@ -358,31 +383,33 @@ toMediaQuery bucket =
 
 
 
-{-| Broad buckets for screen width.
+{-| Custom type representing broad width buckets.
 
-- Handset : The display area can be held in one hand.
-- Portable : The display area is portable, but not a handset.
-- Wide : Usually desktop contexts.
+Check this package's readme for summaries of all the buckets.
 -}
-type WidthBucket
+type BroadWidthBucket
     = Handset
     | Portable
     | Wide
 
-
+{-| Raw values for the handset width bucket.
+-}
 handsetBkt : Bucket
 handsetBkt =    { min = NoLimit
                 , max = handset3Bkt.max
                 , axis = Width
                 }
 
-
+{-| Raw values for the portable width bucket.
+-}
 portableBkt : Bucket
 portableBkt =   { min = portable1Bkt.min
                 , max = portable3Bkt.max
                 , axis = Width
                 }
 
+{-| Raw values for the wide width bucket.
+-}
 wideBkt : Bucket
 wideBkt =   { min = wide1Bkt.min
             , max = NoLimit
@@ -392,16 +419,9 @@ wideBkt =   { min = wide1Bkt.min
 
 
 
-{-| Finer buckets for screen width.
+{-| Custom type representing fine width buckets.
 
-- Handset1 : Tiny phones phones such as Palm, iPhone SE 1st gen. (Usually sub-5")
-- Handset2 : Regular phones. (~5" - 6")
-- Handset3 : Large phones. ( ~6" - 7")
-- Portable1 : Small tablets (~7-8"), narrow desktop windows.
-- Portable2 : Regular tablets in portrait (~10"), small desktop windows.
-- Portable3 : Large tablets in portrait, small and regular tablets in landscape.
-- Wide1 : Large tablets in landscape and budget laptops. 720p, 1366x768, 1280x800.
-- Wide2 : Desktop and large laptop displays. 1680x1050, 1080p, 1440p, 4K.
+Check this package's readme for summaries of all the buckets.
 -}
 type FineWidthBucket
     = Handset1
@@ -413,49 +433,64 @@ type FineWidthBucket
     | Wide1
     | Wide2
 
-
+{-| Raw values for the handset1 width bucket.
+-}
 handset1Bkt : Bucket
 handset1Bkt =   { min = NoLimit
                 , max = stepBelow handset2Bkt
                 , axis = Width
                 }
 
+{-| Raw values for the handset2 width bucket.
+-}
 handset2Bkt : Bucket
 handset2Bkt =   { min = Defined 352 -- 16 * 22
                 , max = stepBelow handset3Bkt
                 , axis = Width
                 }
 
+{-| Raw values for the handset3 width bucket.
+-}
 handset3Bkt : Bucket
 handset3Bkt =   { min = Defined 384 -- 16 * 24
                 , max = stepBelow portable1Bkt
                 , axis = Width
                 }
 
+{-| Raw values for the portable1 width bucket.
+-}
 portable1Bkt : Bucket
 portable1Bkt =  { min = Defined 512 -- 16 * 32
                 , max = stepBelow portable2Bkt
                 , axis = Width
                 }
 
+{-| Raw values for the portable2 width bucket.
+-}
 portable2Bkt : Bucket
 portable2Bkt =  { min = Defined 640 -- 16 * 40
                 , max = stepBelow portable3Bkt
                 , axis = Width
                 }
 
+{-| Raw values for the portable3 width bucket.
+-}
 portable3Bkt : Bucket
 portable3Bkt =  { min = Defined 864 -- 16 * 54
                 , max = stepBelow wide1Bkt 
                 , axis = Width
                 }
 
+{-| Raw values for the wide1 width bucket.
+-}
 wide1Bkt : Bucket
 wide1Bkt =  { min = Defined 1056 -- 16 * 66
             , max = stepBelow wide2Bkt
             , axis = Width
             }
 
+{-| Raw values for the wide2 width bucket.
+-}
 wide2Bkt : Bucket
 wide2Bkt =  { min = Defined 1440 -- 16 * 90
             , max = NoLimit
@@ -467,36 +502,44 @@ wide2Bkt =  { min = Defined 1440 -- 16 * 90
 
 
 
-{-| CSS Media query for the handset1 bucket. -}
+{-| CSS Media query for the handset1 width bucket.
+-}
 handset1 : Media.MediaQuery
 handset1 = toMediaQuery handset1Bkt
 
-{-| CSS Media query for the handset2 bucket. -}
+{-| CSS Media query for the handset2 width bucket.
+-}
 handset2 : Media.MediaQuery
 handset2 = toMediaQuery handset2Bkt
 
-{-| CSS Media query for the handset3 bucket. -}
+{-| CSS Media query for the handset3 width bucket.
+-}
 handset3 : Media.MediaQuery
 handset3 = toMediaQuery handset3Bkt
 
-{-| CSS Media query for the portable1 bucket. -}
+{-| CSS Media query for the portable1 width bucket.
+-}
 portable1 : Media.MediaQuery
 portable1 = toMediaQuery portable1Bkt
 
-{-| CSS Media query for the portable2 bucket. -}
+{-| CSS Media query for the portable2 width bucket.
+-}
 portable2 : Media.MediaQuery
 portable2 = toMediaQuery portable2Bkt
 
 
-{-| CSS Media query for the portable3 bucket. -}
+{-| CSS Media query for the portable3 width bucket.
+-}
 portable3 : Media.MediaQuery
 portable3 = toMediaQuery portable3Bkt
 
-{-| CSS Media query for the wide1 bucket. -}
+{-| CSS Media query for the wide1 width bucket.
+-}
 wide1 : Media.MediaQuery
 wide1 = toMediaQuery wide1Bkt
 
-{-| CSS Media query for the wide2 bucket. -}
+{-| CSS Media query for the wide2 width bucket.
+-}
 wide2 : Media.MediaQuery
 wide2 = toMediaQuery wide2Bkt
 
@@ -524,12 +567,10 @@ wide = toMediaQuery wideBkt
 
 
 {-| Takes an int representing screen width in pixels
-and converts it into a (broad) screen width bucket.
-
-https://github.com/dzuk-mutant/Helium/blob/master/docs/display.md#display-width-groups
+and converts it into a broad screen width bucket.
 -}
-toWidthBucket : Int -> WidthBucket
-toWidthBucket w =
+toBroadWidthBucket : Int -> BroadWidthBucket
+toBroadWidthBucket w =
     if inBucket handsetBkt w then
         Handset
     else if inBucket portableBkt w then
@@ -540,8 +581,6 @@ toWidthBucket w =
 
 {-| Takes a Int representing screen width and 
  and converts it into a fine screen width bucket.
-
-https://github.com/dzuk-mutant/Helium/blob/master/docs/display.md#display-width-groups
 -}
 toFineWidthBucket : Int -> FineWidthBucket
 toFineWidthBucket w =
@@ -575,33 +614,33 @@ toFineWidthBucket w =
 ---------------------- height ----------------------
 
 
-{-| Buckets for screen height.
+{-| Custom type representing height buckets.
 
-There aren't as many as screen width because it has
-generally limited implications for web application design.
-
-- Limited : Handsets in landscape. Very limited screen space.
-- Portable : Small and regular tablets in landscape. Restricted screen space.
-- Wide : Large tablet in landscape, laptop or desktop contexts. Ample screen space.
+Check this package's readme for summaries of all the buckets.
 -}
 type HeightBucket
     = Limited
     | Medium
     | Tall
 
-
+{-| Raw values for the limited height bucket.
+-}
 limitedBkt : Bucket
 limitedBkt =    { min = NoLimit
                 , max = stepBelow mediumBkt
                 , axis = Height
                 }
 
+{-| Raw values for the medium height bucket.
+-}
 mediumBkt : Bucket
 mediumBkt =     { min = Defined 512 -- 16 * 32
                 , max = stepBelow tallBkt
                 , axis = Height
                 }
 
+{-| Raw values for the tall height bucket.
+-}
 tallBkt : Bucket
 tallBkt =   { min = Defined 864 -- 16 * 54
             , max = NoLimit
